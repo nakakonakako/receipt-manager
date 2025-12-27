210c18dae38c6edec1143444bb3c7d00dbada05d
nakakonakako <kotamina2016@gmail.com>
2025-12-27 19:54:15 +0900

    build: プロキシの導入

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,5 +7,13 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: true,
+
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 })
