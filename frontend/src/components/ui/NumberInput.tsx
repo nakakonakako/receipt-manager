fabdf9262b2dc5891bdd3e266e77c0e3f873ec72
nakakonakako <kotamina2016@gmail.com>
2026-02-27 01:54:35 +0900

    feat: 数値入力欄で負の数を扱えるように変更

--- frontend/src/components/ui/NumberInput.tsx
+++ frontend/src/components/ui/NumberInput.tsx
@@ -5,7 +5,7 @@ interface NumberInputProps extends Omit<
   'onChange' | 'value'
 > {
   value: number | ''
-  onChange: (value: number | '') => void
+  onChange: (value: number | string) => void
   maxLength?: number
 }
 
@@ -19,18 +19,26 @@ export const NumberInput: React.FC<NumberInputProps> = ({
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let val = e.target.value
 
-    val = val.replace(/\D/g, '')
+    val = val.replace(/(?!^-)[^0-9]/g, '')
 
-    if (val.length > 1 && val.startsWith('0')) {
-      val = val.replace(/^0+/, '')
-      if (val === '') val = '0'
+    const isNagative = val.startsWith('-')
+    const numPart = val.replace('-', '')
+
+    if (numPart.length > 1 && numPart.startsWith('0')) {
+      val = isNagative
+        ? '-' + numPart.replace(/^0+/, '')
+        : val.replace(/^0+/, '')
     }
 
-    if (maxLength && val.length > maxLength) {
-      val = val.slice(0, maxLength)
+    if (maxLength && numPart.length > maxLength) {
+      val = val.slice(0, isNagative ? maxLength + 1 : maxLength)
     }
 
-    onChange(val === '' ? '' : Number(val))
+    if (val === '-' || val === '') {
+      onChange(val)
+    } else {
+      onChange(Number(val))
+    }
   }
 
   return (
