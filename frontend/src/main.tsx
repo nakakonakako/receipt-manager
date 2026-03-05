bae927dad133e5367be3a23bbfd2fa5b4b716d6a
nakakonakako <kotamina2016@gmail.com>
2026-03-05 23:11:43 +0900

    chore: toastの導入

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App'
 import { AuthProvider } from './contexts/AuthContext'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
+      <Toaster position="top-center" richColors />
       <App />
     </AuthProvider>
   </StrictMode>
